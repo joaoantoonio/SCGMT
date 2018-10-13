ae724d0b8c63d417a9fba25e77f4147412f070e6
João Antônio Silva Ramos <42175944+joaoantoonio@users.noreply.github.com>
2018-10-13 14:11:48 -0300

    Add files via upload

---
+++ EquipamentoDTO.cs
@@ -0,0 +1,56 @@
+﻿namespace Projeto3Camadas.Código.DTO
+{
+    // A classe foi criada para inserirmos nela os atributos do objeto Equipamento, os atributos que criamos são os campos do Frm_CadastroEquipamento.
+
+    public class EquipamentoDTO
+    {
+        /// <summary>
+        /// Id para identificar um registro da tabela equipamento.
+        /// </summary>
+        public int id_equipamento { get; set; }
+
+        /// <summary>
+        /// Monobloco do equipamento.
+        /// </summary>
+        public string monobloco { get; set; }
+
+        /// <summary>
+        /// Motor do equipamento.
+        /// </summary>
+        public string motor { get; set; }
+
+        /// <summary>
+        /// Série do equipamento.
+        /// </summary>
+        public string serie { get; set; }
+
+        /// <summary>
+        /// Horas do equipamento.
+        /// </summary>
+        public decimal horimetro { get; set; }
+
+        /// <summary>
+        /// Nota Fiscal de Compra do equipamento.
+        /// </summary>
+        public string nf_compra { get; set; }
+
+        /// <summary>
+        /// Código do proprietário do equipamento.
+        /// </summary>
+        public int fk_proprietario { get; set; }
+
+        /// <summary>
+        /// Construtora padrão.
+        /// </summary>
+        public EquipamentoDTO()
+        {
+            id_equipamento = 0;
+            monobloco = string.Empty;
+            motor = string.Empty;
+            serie = string.Empty;
+            horimetro = 0;
+            nf_compra = string.Empty;
+            fk_proprietario = 0;
+        }
+    }
+}
---
+++ M500HorasDTO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Projeto3Camadas.Código.DTO
+{
+    // A classe foi criada para inserirmos nela os atributos do objeto Manutenção de 500 Horas, os atributos que criamos são os campos do Frm_Manutenção500Horas.
+
+    public class M500HorasDTO
+    {
+        /// <summary>
+        /// Id para identificar um registro da tabela de manutenção de 500 horas.
+        /// </summary>
+        public int id_manut_500_horas { get; set; }
+
+        /// <summary>
+        /// Código do equipamento.
+        /// </summary>
+        public int fk_equipamento { get; set; }
+
+        /// <summary>
+        /// Código do responsável técnico.
+        /// </summary>
+        public int fk_resp_tecnico { get; set; }
+
+        /// <summary>
+        /// Horas do equipamento.
+        /// </summary>
+        public decimal horimetro { get; set; }
+
+        /// <summary>
+        /// Data em que a manutenção foi executada.
+        /// </summary>
+        public DateTime data_manutencao { get; set; }
+
+        /// <summary>
+        /// CPF do responsável técnico.
+        /// </summary>
+        public string cpf_resp_tecnico { get; set; }
+
+        /// <summary>
+        /// Construtora padrão
+        /// </summary>
+        public M500HorasDTO()
+        {
+            id_manut_500_horas = 0;
+            fk_equipamento = 0;
+            fk_resp_tecnico = 0;
+            horimetro = 0;
+            data_manutencao = DateTime.MinValue;
+            cpf_resp_tecnico = string.Empty;
+        }
+    }
+}
